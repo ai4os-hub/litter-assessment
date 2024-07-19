4dc9519b7697d8a3422e3b667888375d11da05fa
root <valentin.kozlov@gmail.com>
2024-07-19 11:55:51 +0000

    fix: move tests up, add test_get_metadata, add folder for test images

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,42 +8,53 @@ as expected.
 
 These tests will run in the Jenkins pipeline after each change
 you make to the code.
+
+It is good to use either unittest or pytest 
 """
 import sys
 import os
 import shutil
+import unittest
 
 from deepaas.model.v2.wrapper import UploadedFile
 from litter_assessment_service import api
 
+BASE_PATH = os.path.dirname(os.path.normpath(os.path.dirname(__file__)))
+TEST_PATH = os.path.join(BASE_PATH, "tests")
+
+def test_get_metadata():
+    meta = api.get_metadata()
+    assert type(meta) is dict
+
 def test_predict_single_im():
-    test_data_path=os.path.join(sys.path[-1], 'data','samples')
+    test_data_path=os.path.join(TEST_PATH,'data','samples')
     file_path=os.path.join(test_data_path, 'test_image.JPG')
     tmp_fpath = os.path.join(test_data_path, 'tmp_file.jpg')
     shutil.copyfile(file_path, tmp_fpath)
     file = UploadedFile(name='data', filename=tmp_fpath, content_type='image/jpg', original_filename='test_image')
-    kwargs = {'input': file, 'PLD_plot': True, 'PLQ_plot': True}
+    kwargs = {'files': file, 'PLD_plot': True, 'PLQ_plot': True}
     api.predict(**kwargs)
 
 def test_predict_zip():
-    test_data_path=os.path.join(sys.path[-1], 'data','samples')
+    test_data_path=os.path.join(TEST_PATH, 'data','samples')
     file_path=os.path.join(test_data_path, 'samples.zip')
     tmp_fpath = os.path.join(test_data_path, 'tmp_file.zip')
     shutil.copyfile(file_path, tmp_fpath)
     file = UploadedFile(name='data', filename=tmp_fpath, content_type='application/zip', original_filename='test_zip')
-    kwargs = {'input': file, 'PLD_plot': True, 'PLQ_plot': True}
+    kwargs = {'files': file, 'PLD_plot': True, 'PLQ_plot': True}
     api.predict(**kwargs)
 
 def test_predict_bytes():
-    test_data_path=os.path.join(sys.path[-1], 'data','samples')
+    test_data_path=os.path.join(TEST_PATH, 'data','samples')
     file_path=os.path.join(test_data_path, 'test_image.JPG')
     image=open(os.path.join(os.getcwd(), 'test_images', file_path), 'rb')
     bin_image = image.read()
     file = UploadedFile(name='data', filename=bin_image, content_type='application/octet-stream', original_filename='test_image.JPG')
-    kwargs = {'input': file, 'PLD_plot': True, 'PLQ_plot': True}
+    kwargs = {'files': file, 'PLD_plot': True, 'PLQ_plot': True}
     api.predict(**kwargs)
 
 def main():
+    test_get_metadata()
     test_predict_bytes()
     test_predict_zip()
     test_predict_single_im()
