59e377fae4bb66c572d74de085b6201dbe96ba57
Carolin Leluschko <carolin.leluschko@dfki.de>
2024-07-30 11:15:56 +0200

    fix: path to configs.yaml

--- litter_assessment_service/preprocessing.py
+++ litter_assessment_service/preprocessing.py
@@ -44,7 +44,7 @@ def load_configs(plot_type):
     returns colors and labels for plot as specified in config.yaml file
     """
     wd = os.getcwd()
-    path = os.path.join(wd, 'litter_assessment_service/litter_assessment_service/configs.yaml')
+    path = os.path.join(wd, 'litter-assessment/litter_assessment_service/configs.yaml')
     with open(path, 'rb') as f:
         params = yaml.safe_load(f)
         color = params['plot params'][f'colors {plot_type}']
