1742b853a8d4290b7c92bb01b7efb84bc451bff1
Carolin Leluschko <carolin.leluschko@dfki.de>
2024-08-05 08:52:19 +0200

    fix: download model before running tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,6 +15,9 @@ import sys
 import os
 import shutil
 import unittest
+import requests
+import tarfile
+import logging
 
 from deepaas.model.v2.wrapper import UploadedFile
 from litter_assessment_service import api
@@ -54,7 +57,39 @@ def test_predict_bytes():
     kwargs = {'files': file, 'PLD_plot': True, 'PLQ_plot': True}
     api.predict(**kwargs)
 
+def download_file(url):
+    local_filename = url.split('/')[-1] # OR add the path, e.g. BASE_PATH/models
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                #if chunk: 
+                f.write(chunk)
+    return local_filename
+
 def main():
+    # just python logging
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+    # URL to the models file
+    models_url = "https://share.services.ai4os.eu/index.php/s/HQmXS7mcDK82sz3/download/models.tar.gz"
+
+    try:
+    # call download_file function
+        models_archived = download_file(models_url)
+
+        # dearchive downloaded .tar.gz file
+        with tarfile.open(models_archived, 'r:gz') as ms:
+            ms.extractall("./") # OR use BASE_PATH/models
+
+        # may delete the *tar.gz afterwards
+        os.remove(models_archived)
+    except Exception as err:
+        logging.error(f"Unexpected {err=}, {type(err)=}")
+
     test_get_metadata()
     test_predict_bytes()
     test_predict_zip()
